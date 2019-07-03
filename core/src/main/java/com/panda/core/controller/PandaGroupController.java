a3bc80c9ccc9aa1e35cbb75d4bdad901f1b30c57
zhanglijian <zhanglijian@vipkid.com.cn>
2019-07-03 11:06:35 +0800

    init

--- core/src/main/java/com/panda/core/controller/PandaGroupController.java
+++ core/src/main/java/com/panda/core/controller/PandaGroupController.java
@@ -1,12 +1,19 @@
 package com.panda.core.controller;
 
 
+import com.panda.common.dto.SelectItemDto;
+import com.panda.common.util.SelectItemUtil;
 import com.panda.core.consts.CoreConst;
+import com.panda.core.dto.PandaAppDto;
 import com.panda.core.dto.PandaGroupDto;
 import com.panda.core.dto.search.PandaGroupSo;
 import com.panda.core.entity.PandaGroup;
+import com.panda.core.service.IPandaBusinessLineService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * <p>
@@ -20,4 +27,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(CoreConst.MAIN_REQUEST_PREFIX + "group")
 public class PandaGroupController extends BaseController<PandaGroup, PandaGroupDto, PandaGroupSo> {
 
+
+    @Autowired
+    private IPandaBusinessLineService iPandaBusinessLineService;
+
+    @Override
+    protected List<PandaGroupDto> decorateList(List<PandaGroupDto> list) {
+        List<SelectItemDto> lines = iPandaBusinessLineService.selectItem(false);
+        for (PandaGroupDto dto : list) {
+            dto.setBusinessLineName(SelectItemUtil.getValueById(dto.getBusinessLineId(), lines));
+        }
+        return super.decorateList(list);
+    }
 }
