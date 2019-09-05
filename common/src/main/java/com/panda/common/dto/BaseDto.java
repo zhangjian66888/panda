4865f89f7a11ff0cfe83262b6697c7e54338e49b
zhanglijian <zhanglijian@vipkid.com.cn>
2019-09-05 12:28:57 +0800

    panda

--- common/src/main/java/com/panda/common/dto/BaseDto.java
+++ common/src/main/java/com/panda/common/dto/BaseDto.java
@@ -1,5 +1,6 @@
 package com.panda.common.dto;
 
+import com.google.common.collect.Lists;
 import com.panda.common.mybatis.InPair;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,6 +8,7 @@ import lombok.Setter;
 import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.Objects;
+import java.util.Optional;
 
 /**
  * com.panda.common.dto.BaseDto
@@ -27,6 +29,10 @@ public abstract class BaseDto {
 
     protected Collection<InPair> ins;
 
+    public Collection<InPair> getIns() {
+        this.ins = Optional.ofNullable(ins).orElse(Lists.newArrayList());
+        return ins;
+    }
     public BaseDto in(String column, Collection collection) {
         if (Objects.nonNull(collection) && !collection.isEmpty()) {
             getIns().add(InPair.builder().column(column).values(collection).build());
